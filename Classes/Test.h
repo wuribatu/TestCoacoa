c171e4eee233e6c5b5580d51d6d58bdbdfbcad77
batu <wuribatu_iphone@yeah.net>
2018-06-11 14:50:36 +0800

    123

--- Classes/Test.h
+++ Classes/Test.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  Test.h
 //  11
 //
 //  Created by batu on 2018/6/11.
 //  Copyright © 2018年 Smedia Education Technology Co., Ltd. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-
+@interface Test : NSObject
 
 @end
-
---
+++ Classes/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  11
+//
+//  Created by batu on 2018/6/11.
+//  Copyright © 2018年 Smedia Education Technology Co., Ltd. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
